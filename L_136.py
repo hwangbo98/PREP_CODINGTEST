6e4e9e9e895eb3b51ba01598b1e2134e876879a8
hwangbo98 <hwangbo1998@gmail.com>
2022-08-13 00:11:08 +0900

    Leetcode 136 Single Number

---
+++ L_136.py
@@ -0,0 +1,8 @@
+#Single Number : https://leetcode.com/problems/single-number/ LeetCode 136
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result = list(set(nums))
+        
+        for i in result :
+            if(nums.count(i) == 1) :
+                return i
