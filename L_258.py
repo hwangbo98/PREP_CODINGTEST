1f79077190398c4ed3372fdb1c23268d61cb4a9f
hwangbo98 <hwangbo1998@gmail.com>
2022-08-17 23:33:00 +0900

    20220817

---
+++ L_258.py
@@ -0,0 +1,12 @@
+# Leetcode adddigit 258번 https://leetcode.com/problems/add-digits/
+class Solution:
+    def addDigits(self, num: int) -> int:
+        int_to_str = str(num)
+        result = 0
+        while(len(int_to_str) >= 2) :
+            for i in int_to_str :
+                result += int(i)
+            int_to_str = str(result)
+            result = 0
+        
+        return int(int_to_str)
