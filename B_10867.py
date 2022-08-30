6a13f4c342e5320dd0f0db307f1900bd2ec3ef67
hwangbo98 <hwangbo1998@gmail.com>
2022-08-31 01:06:29 +0900

    20220831

--- B_10867.py
+++ B_10867.py
@@ -5,11 +5,6 @@ list_num = map(int, input().split())
 
 result = list(set(list_num))
 result.sort()
-print(result)
 
-count = 0
 for i in result :
-    if(count == len(result)) :
-        print(i)
-        
     print(i, end = " ")
